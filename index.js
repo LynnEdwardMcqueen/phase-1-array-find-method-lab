c741dd33443dd7e8dbd70f7144bec8b839ace55d
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-03-31 19:11:03 -0600

    adds data analysis for the glory of the Broncos

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superBowlWinCheck( yearInfo)  {
+    return (yearInfo.result === "W");
+  }
+
+  function superbowlWin ( record) {
+    let winningInfo;
+    winningInfo = record.find(superBowlWinCheck);
+    if (winningInfo != undefined) {
+        return winningInfo.year;
+    } else {
+        return undefined;
+    }
+  }
+
+  console.log(superbowlWin(record));
